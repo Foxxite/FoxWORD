56666318ee5ea03e919078652705a00f0e657407
Foxxite <foxxite@foxxite.com>
2019-11-27 11:07:33 +0100

    Added Crypto JS Package

--- assets/animated-icons/animated-icons.js
+++ assets/animated-icons/animated-icons.js
@@ -1,21 +1,25 @@
+var scripts = document.getElementsByTagName('script');
+var path = scripts[scripts.length-1].src.split('?')[0];
+var mydir = path.split('/').slice(0, -1).join('/')+'/';
+
 window.onload = function() {
     if (!window.jQuery) {  
         throw new Error('jQuery is required for animated icons to work!');
     }
 }
 
-$(doucment).ready(function() {
+$(document).ready(function() {
 
-    var scriptPath = document.currentScript;
-    alert(scriptPath);
+    var scriptPath = mydir;
+    console.log(scriptPath);
 
     $("i.anic").each(function() {
 
         var iconName = $(this).data("icon");
 
-        var img = "<svg src='' ";
+        var img = "<img src='"+scriptPath+"/"+iconName+"/"+iconName+".gif'></img>";
 
-        $(this).html();
+        $(this).html(img);
 
     });
 

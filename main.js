506b246bc6d84acbae2034fa9b3ec610d7aaf680
Foxxite <foxxite@foxxite.com>
2019-11-27 17:50:52 +0100

    More styling

--- main.js
+++ main.js
@@ -18,10 +18,14 @@ function createWindow () {
 	mainWindow = new BrowserWindow({
 		width: 1280,
 		height: 720,
+		minHeight: 300,
+  		minWidth: 775,
+		useContentSize: true,
+		fullscreen: false,
 		icon: __dirname + '/assets/img/fox.png',
 		titleBarStyle: 'hiddenInset',
 		backgroundColor: '#FFF',
-		show: true,
+		show: false,
 		webPreferences: {
 		  nodeIntegration: true
 		}
@@ -37,7 +41,7 @@ function createWindow () {
 
 	//Show window when ready
 	mainWindow.once('ready-to-show', () => {
-		mainWindow.show()
+		mainWindow.show();
 	})
 
 	// Emitted when the window is closed.
@@ -45,7 +49,7 @@ function createWindow () {
 		// Dereference the window object, usually you would store windows
 		// in an array if your app supports multi windows, this is the time
 		// when you should delete the corresponding element.
-		mainWindow = null
+		mainWindow = null;
 	})
 }
 
@@ -59,7 +63,7 @@ app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
 })
 
@@ -67,7 +71,7 @@ app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
 })
 
--- renderer.js
+++ renderer.js
@@ -7,9 +7,47 @@ const { remote, BrowserWindow } = require('electron')
 const app = remote.app;
 const currentWindow = remote.getCurrentWindow()
 
+$("#content").hide();
+
 //All page code
 $(document).ready(function() {
 	
 	console.log(app.getPath('userData'));
+
+	//Main App Logic
+	$("#logo_nav").click(function() {
+
+		$(".navside").toggle("drop", { direction: 'left', mode: 'toggle' }, 500);
+		
+		if($(".navside").visible())
+		{
+			$(".main-content").css("left", "0px");
+		}
+		else
+		{
+			$(".main-content").css("left", "150px");
+		}
+
+	});
+
+
+	//Insert logic to load right page on first start
+
+	$.get('pages/home.html')
+	.done(function(data) {
+		$("#content").show("drop", { direction: 'right', mode: 'show' }, 500);
+		$("#content").html(data);
+	})
+	.fail(function(jqXHR, textStatus, errorThrown) {
+		var errorMessage;
+
+		if (textStatus == 'timeout')
+			errorMessage = 'The server is not responding';
+
+		if (textStatus == 'error')
+			errorMessage = textStatus + " " + errorThrown;
+
+		$("#content").html("<h1>An internal error occured.</h1><h3>"+errorMessage+"</h3>");
+	});
 	
 });

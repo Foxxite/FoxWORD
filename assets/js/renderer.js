ccda4aac3e762954ea2da280cb917e5daee4dd6f
Foxxite <foxxite@foxxite.com>
2019-11-29 17:35:09 +0100

    More Structure Changes

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -7,12 +7,29 @@ const { remote, BrowserWindow } = require('electron')
 const app = remote.app;
 const currentWindow = remote.getCurrentWindow()
 
+const fs = require('fs')
+
 $("#content").hide();
 
 //All page code
 $(document).ready(function() {
 	
 	console.log(app.getPath('userData'));
+
+	//Figure Out If First Launch & Decrypt Database
+
+	const basePath = app.getPath('userData');
+
+	fs.access(basePath + "/database.foxword", fs.F_OK, (err) => {
+		if (err) {
+			setupNewUser();
+		}
+		else
+		{
+			setupUser();
+		}
+	})
+
 
 	//Show firts load animations
 	$(".navbar").hide();
@@ -23,8 +40,33 @@ $(document).ready(function() {
 	setTimeout(function () { $("body").css("background-color", "white"); }, 200);
 
 	//Render Dynamic Launch Content
-	$("#version").html("<i class='fas fa-code-branch'></i> Version: " + app.getVersion());
+	$("#version span").html(app.getVersion());
 
+});
+
+function setupNewUser()
+{
+	$("body").hide();
+	$.get('pages/newuser.html')
+	.done(function(data) {
+		$("body").html(data);
+		$("body").show("drop", { direction: 'up', mode: 'show' }, 500);
+	})
+	.fail(function(jqXHR, textStatus, errorThrown) {
+		var errorMessage;
+
+		if (textStatus == 'timeout')
+			errorMessage = 'The server is not responding';
+
+		if (textStatus == 'error')
+			errorMessage = textStatus + " " + errorThrown;
+
+		$("#content").html("<h1>An internal error occured.</h1><h3>"+errorMessage+"</h3>");
+	});
+}
+
+function setupUser()
+{
 	//Insert logic to load right page on first start
 	$.get('pages/home.html')
 	.done(function(data) {
@@ -42,5 +84,4 @@ $(document).ready(function() {
 
 		$("#content").html("<h1>An internal error occured.</h1><h3>"+errorMessage+"</h3>");
 	});
-	
-});
+}
